0af5186d281eb820866272b4c8096e2497800fb4
Sushant1702 <97727281+Sushant1702@users.noreply.github.com>
2022-01-26 23:04:50 +0530

    Add files via upload

---
+++ Asg2_21103117_output.py
@@ -0,0 +1,48 @@
+<A>THE LENGTH OF STRING IS 35
+<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP
+<E>THE INDEX OF SUBSTRING a is  10
+<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage
+
+
+ENTER YOUR NAMEankitbhalla
+ENTER YOUR SID21103050
+ENTER YOUR DEPARTMENTcse
+ENTER YOUR CGPA9.7
+Hey ankitbhalla, Here!
+My SID is 21103050
+I am from cse and my CGPA is 9.700000
+
+
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+
+Enter first number: 55
+Enter second number: 54
+Enter third number: 60
+THE LARGEST NUMBER IS  60
+
+
+ENTER A STRINGname
+Yes
+
+
+ENTER FIRST SIDE OF TRIANGLE5
+ENTER SECOND SIDE OF TRIANGLE6
+ENTER THIRD SIDE OF TRIANGLE8
+Yes
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
